788e20a77efc4c5397be5e85afb7d264e7305061
Karol <cuaox@localhost.localdomain>
2017-03-08 19:02:05 +0000

    Starting again

--- BOOTLOADER/stage1.asm
+++ BOOTLOADER/stage1.asm
@@ -1,27 +1,13 @@
 use16
 org 0x7c00
+jmp start
+	include 'INC/gdt.inc'	
 
-jmp far 0x0000:start
 
 start:
-	mov ax, 0x2000
-	mov es, ax
-	xor bx, bx 
-
-	;preparing to read stage 2 sectors
-	mov ah, 2
-	mov al, 72 ;read 72 sectors
-	mov ch, 0
-	mov cl, 2
-	mov dh, 0
-	;dl set
-
-	
-	int 0x13
-	
-	jmp far 0x2000:0x0000
+	call create_gdt
+	jmp $
 
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
-
