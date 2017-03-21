7a74493d907d05186ffa3d86c7e795e89bb34642
cuaox <szustarol@gmail.com>
2017-03-21 12:54:22 +0100

    Memory map

--- BOOTLOADER/stage2.asm
+++ BOOTLOADER/stage2.asm
@@ -5,20 +5,15 @@ jmp 0x0000:stage2_start
     include 'INC/a20.inc'
     include 'INC/gdt.inc'
     include 'INC/vesa.inc'
+    include 'INC/mmap.inc'
 
 stage2_start:
     call a20_activation
     call run_in_vesa
+    call memory_map
     call create_gdt
     ;since other calls might modify GDT (for example VESA)
     ;make sure to call create_gdt as last call before 32-bit mode
-    mov eax, 512
-    mov ebx, 368
-    call get_pixel_position
-    mov dword [ecx], 0x00ff00ff
-    .lf:
-        nop
-        nop
-        jmp .lf
+    jmp $
 
     FRAMEBUFFER_ADR dd 0
