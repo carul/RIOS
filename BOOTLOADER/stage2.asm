bcdfe66849c1fce8b12060b4e988795658142698
cuaox <szustarol@gmail.com>
2017-03-22 19:03:32 +0100

    32-bit mode and HIS set up

--- BOOTLOADER/stage2.asm
+++ BOOTLOADER/stage2.asm
@@ -9,11 +9,26 @@ jmp 0x0000:stage2_start
 
 stage2_start:
     call a20_activation
-    call run_in_vesa
+    call zero_his
+    call run_in_vesa;be carefull and keep track of di now, since we are going to store memory map output into it
     call memory_map
     call create_gdt
     ;since other calls might modify GDT (for example VESA)
     ;make sure to call create_gdt as last call before 32-bit mode
-    jmp $
+    jmp e_o_s2
 
-    FRAMEBUFFER_ADR dd 0
+
+zero_his: ;we will create a hardware info structure, which will contain:
+;1. Video mode settings- screen width, framebuffer addr, pitch, scr. height and color depth
+;2. All valid returns from memory map e820 call, unosrted, the list will be finished by 
+;SYS_EOIS string, which will be added in the mmap.inc file
+;this function will just zero memory arund 0x500, just in case
+    cld
+    mov di, 0x500
+    mov al, 0
+    mov cx, 0x1000
+    rep stosb
+    ret
+
+
+e_o_s2:
--- BOOTLOADER/stage3.asm
+++ BOOTLOADER/stage3.asm
@@ -1,1 +1,4 @@
 ;;this file will be used to setup 64-bit mode, paging ETC
+stage3:
+    use32
+    jmp $
