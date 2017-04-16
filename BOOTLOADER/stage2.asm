7874165879902288457e24e9f20901ad3913e4ea
Cuaox <szustarol@gmail.com>
2017-04-16 18:28:39 +0200

    a bit into paging

--- BOOTLOADER/stage2.asm
+++ BOOTLOADER/stage2.asm
@@ -13,7 +13,7 @@ stage2_start:
     call run_in_vesa;be carefull and keep track of di now, since we are going to store memory map output into it
     call memory_map
     call create_gdt
-    
+
     ;since other calls might modify GDT (for example VESA)
     ;make sure to call create_gdt as last call before 32-bit mode
     cli
@@ -27,7 +27,7 @@ stage2_start:
 
 zero_his: ;we will create a hardware info structure, which will contain:
 ;1. Video mode settings- screen width, framebuffer addr, pitch, scr. height and color depth
-;2. All valid returns from memory map e820 call, unosrted, the list will be finished by 
+;2. All valid returns from memory map e820 call, unosrted, the list will be finished by
 ;SYS_EOIS string, which will be added in the mmap.inc file
 ;this function will just zero memory arund 0x500, just in case
     cld
@@ -42,7 +42,7 @@ zero_his: ;we will create a hardware info structure, which will contain:
 
 e_o_s2:
     use32
-    mov ax, 0x10 
+    mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov ss, ax
@@ -54,19 +54,77 @@ e_o_s2:
     cmp ecx, 0
     jg .l
     ;if screen gets green - we are in real mode!
-           
 
-    jmp $
-longmode:
-    jmp $
+    ;;Now lets set up paging
+    lea eax, [PML4]
+    mov cr3, eax
 
-    use64
-    mov ax, 0x10
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    jmp $
+    mov eax, cr4
+    or eax, 100000b
+    mov cr4, eax
 
- 
+    mov ecx, 0xc0000080
+    rdmsr
+    or eax, 100000000b
+    wrmsr
+
+    mov eax, cr0
+    mov ebx, 0x1
+    shl ebx, 31
+    or eax, ebx
+    mov cr0, eax
+
+    call gdt64_install
+    push 8
+    push longmode
+    retf
+
+
+    gdt64_install:
+        lgdt[GDT_addr]
+        ret
+
+
+        GDT_addr:
+        dw (GDT64_end - GDT64) - 1
+        dd GDT64
+
+        GDT64:
+        dd 0, 0
+
+        dd 0xffff  ; segment limit
+        dd 0xef9a00
+
+        dd 0xffff  ; segment limit
+        dd 0xef9200
+
+        dd 0, 0
+        GDT64_end:
+
+
+align 4096 ;;align to 4 KB
+    PML4:
+        dq 0 or 1b or 10b or PDP;;preset bit, r/w bit
+        dq 511 dup(PDP or 10b)
+    PDP:
+        dq 0 or 1b or 10000000b ;;dq zero, because we map memory from start so 0x0000, present bit
+        ;;PDPE.PS to indicate 1gb pages
+        dq 511 dup(10000000b)
+
+;;1 GB of memory is mapped statically, rest will be allocated in 64 bit mode
+
+;;include 'INC/gdt64.inc'
+
+
+
+    longmode:
+        use64
+        mov ax, 0x10
+        mov ds, ax
+        mov es, ax
+        mov fs, ax
+        mov gs, ax
+        mov ss, ax
+        jmp $
+
+jmp $
--- Makefile
+++ Makefile
@@ -21,6 +21,7 @@ deploy_bootloader: compile_bootloader
 	cat ./BOOTLOADER/stage1.bin ./BOOTLOADER/stage2.bin > floppy.bin
 
 compile_bootloader: ./BOOTLOADER/stage1.asm ./BOOTLOADER/stage2.asm
+	fasm ./BOOTLOADER/stage1.asm
 	fasm ./BOOTLOADER/stage2.asm
 
 clean_temp:
